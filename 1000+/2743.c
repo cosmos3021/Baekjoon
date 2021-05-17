bc7611da4950fd11ec67393b488471275df65711
cosmos3021 <cosmos3021@gmail.com>
2021-05-17 17:52:31 +0900

    Added 1000.c and 2743.c

---
+++ 1000+/2743.c
@@ -0,0 +1,13 @@
+//น้มุ 2743
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char aaa[101] = { 0 };
+	scanf("%s", aaa);
+	int len = strlen(aaa);
+	printf("%d", len);
+	return 0;
+}
