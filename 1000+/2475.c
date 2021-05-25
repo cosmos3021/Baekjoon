672cb545e25f9731746869d13afd933b3466ddde
cosmos3021 <cosmos3021@gmail.com>
2021-05-25 22:01:56 +0900

    Added 2475.c

---
+++ 1000+/2475.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c, d, e, f;
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    f = (a * a + b * b + c * c + d * d + e * e) % 10;
+    printf("%d", f);
+}
