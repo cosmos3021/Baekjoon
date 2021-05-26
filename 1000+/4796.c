3ff8d21d234ba641ec8df33ba30bf608af7f6bce
cosmos3021 <cosmos3021@gmail.com>
2021-05-26 19:46:27 +0900

    Added 4796.c

--- 1000+/4796.c
+++ 1000+/4796.c
@@ -3,19 +3,15 @@
 
 int main(void) {
 	int l, p, v, sum = 0, cnt = 1;
-
 	while (1) {
 		scanf("%d %d %d", &l, &p, &v);
 		if (l == 0) break;
-
 		while (v >= p) {
 			v -= p;
 			sum += l;
 		}
-
 		if (v < l) sum += v;
 		else sum += l;
-
 		printf("Case %d: %d\n", cnt, sum);
 		cnt++, sum = 0;
 	}
