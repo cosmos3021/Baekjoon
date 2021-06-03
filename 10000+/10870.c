cf23b9f431732669850053405de4e28eca50ec8b
cosmos3021 <cosmos3021@gmail.com>
2021-06-03 13:40:20 +0900

    Added 10870.c

--- 10000+/10870.c
+++ 10000+/10870.c
@@ -3,15 +3,12 @@
 
 int main(void) {
 	int i, n, befo = 1, befo2 = 0, befo2_c = 0;
-
 	scanf("%d", &n);
-
 	for (i = 0; i < n - 1; i++) {
 		befo2_c = befo2;
 		befo2 = befo;
 		befo += befo2_c;
 	}
-
 	if (n == 0) befo = 0;
 	printf("%d", befo);
 	return 0;
