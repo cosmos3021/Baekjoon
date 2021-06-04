1d95e9d813839aee321d238446ee291d9b809af1
cosmos3021 <cosmos3021@gmail.com>
2021-06-05 00:07:01 +0900

    Added 100093.c

---
+++ 10000+/10093.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	long long a, b, tmp;
+	scanf("%lld %lld", &a, &b);
+	if (b > a) {
+		tmp = b;
+		b = a;
+		a = tmp;
+	}
+	if (a == b) printf("0\n");
+	else printf("%lld\n", a - b - 1);
+	for (long long i = b + 1; i < a; i++) {
+		printf("%lld ", i);
+	}
+	return 0;
+}
