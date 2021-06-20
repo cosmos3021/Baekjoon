02d2b2608d2dd32f213f190d34094ee7dd23a65e
cosmos3021 <55911892+cosmos3021@users.noreply.github.com>
2021-06-20 20:33:20 +0900

    Added 10988.c

--- 10000+/10988.c
+++ 10000+/10988.c
@@ -6,12 +6,14 @@ int main(void) {
 	char arr[101] = { 0 };
 	scanf("%s", arr);
 	int len = strlen(arr), pel = 1;
+
 	for (int i = 0; i < len; i++) {
 		if (arr[i] != arr[len - 1 - i]) {
 			pel = 0;
 			break;
 		}
 	}
+
 	printf("%d\n", pel);
 	return 0;
 }
