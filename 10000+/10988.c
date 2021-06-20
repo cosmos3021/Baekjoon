d7c6e64a38f094b035cd2de6d6c79c0ad7433abe
cosmos3021 <55911892+cosmos3021@users.noreply.github.com>
2021-06-20 20:31:28 +0900

    Added 10998.c

---
+++ 10000+/10988.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char arr[101] = { 0 };
+	scanf("%s", arr);
+	int len = strlen(arr), pel = 1;
+	for (int i = 0; i < len; i++) {
+		if (arr[i] != arr[len - 1 - i]) {
+			pel = 0;
+			break;
+		}
+	}
+	printf("%d\n", pel);
+	return 0;
+}
