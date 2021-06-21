f00200a0ed748217561e4f2dad04206bd52a6dd9
cosmos3021 <55911892+cosmos3021@users.noreply.github.com>
2021-06-21 13:26:53 +0900

    Added 2442.cpp

---
+++ 1000+/2442.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int n, gong, star;
+	cin >> n;
+	gong = n - 1, star = 1;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < gong; j++) cout << " ";
+		for (int j = 0; j < star; j++) cout << "*";
+		cout << endl;
+		gong--, star += 2;
+	}
+	return 0;
+}
